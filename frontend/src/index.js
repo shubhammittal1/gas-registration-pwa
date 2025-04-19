4e8cbaf421d8057cb232b4f07c3d7fd01dc5b746
shubhammittal1 <105602876+shubhammittal1@users.noreply.github.com>
2025-04-19 14:12:26 +0530

    index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/styles.css';
-import * as serviceWorker from './ServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
